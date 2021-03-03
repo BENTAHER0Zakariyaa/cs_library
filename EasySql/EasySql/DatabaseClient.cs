eecdba51ef8d5beab29e9ce74d5953ee4d0e55d4
Benater zakariyaa <Benater.zakariyaa@ofppt-edu.ma>
2021-03-03 18:10:09 +0100

    .....

---
+++ EasySql/EasySql/DatabaseClient.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EasySql
+{
+    public class DatabaseClient : Sql
+    {
+        public DatabaseClient() : base()
+        { }
+        public DatabaseClient(string stringConnection) : base(stringConnection)
+        { }
+
+        //Open
+        public void Open()
+        {
+            if (this.Connection.State.Equals(ConnectionState.Open))
+            {
+                this.Close();
+                this.Connection.Open();
+            }
+            else
+                this.Connection.Open();
+        }
+
+        //Close
+        public void Close()
+        {
+            this.Connection.Close();
+        }
+        private void ExecuteReader(string stringConnection)
+        {
+            this.setCommand(stringConnection);
+            this.Reader = this.Command.ExecuteReader();
+        }
+
+        private void ExecuteNonQuery(string stringConnection)
+        {
+            this.setCommand(stringConnection);
+            this.Command.ExecuteNonQuery();
+        }
+
+        private Object ExecuteScalar(string stringConnection)
+        {
+            this.setCommand(stringConnection);
+            return this.Command.ExecuteScalar();
+        }
+    }
+}
---
+++ EasySql/EasySql/Sql.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EasySql
+{
+    public class Sql
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+
+        //Data OutPut
+        private DataTable table;
+        private SqlDataReader reader;
+
+
+        internal Sql()
+        {
+            this.connection = new SqlConnection();
+        }
+
+        internal Sql(string stringConnection)
+        {
+            this.connection = new SqlConnection(stringConnection);
+        }
+
+        public SqlConnection Connection { get => this.connection; }
+        public SqlCommand Command { get => this.command; }
+
+        public DataTable Table { get => table; set => table = value; }
+        public SqlDataReader Reader { get => reader; set => reader = value; }
+
+        public void setCommand(string stringQuery)
+        {
+            this.command = new SqlCommand(stringQuery, this.connection);
+        }
+        
+
+    }
+}
